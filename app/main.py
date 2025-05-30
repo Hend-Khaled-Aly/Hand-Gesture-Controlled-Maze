f3068b3bdafb0c5645bb247522ecc97ed2cd524b
Hend Khaled Aly <hendkhaledaly179@gmail.com>
2025-05-30 15:57:14 +0300

    Monitoring Metrics

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model import GestureModel
+from prometheus_fastapi_instrumentator import Instrumentator
+import time
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 model = GestureModel(
     model_path="models/XGBoost_Best_model.pkl",
@@ -16,8 +20,13 @@ class Keypoints(BaseModel):
 
 @app.post("/predict")
 def predict(keypoints: Keypoints):
+    start_time = time.time()
     if len(keypoints.data) != 63:
         return {"error": "Expected 63 features"}
     
     direction = model.predict(keypoints.data)
+
+    latency = time.time() - start_time
+    print(f"Prediction latency: {latency:.4f} seconds")
+    
     return {"direction": direction}
